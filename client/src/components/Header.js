390a1099e4cc7807d5ea849be6a9f4ae7dbd775e
Stephen Shilale <sshilale@thomasnet.com>
2018-03-02 12:41:55 -0500

    applied background to active nav

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import logo from '../ql.logo.png';
+import RaisedButton from 'material-ui/RaisedButton';
 
 export default class EntryAdder extends React.Component {
     render() {
         return (
             <div>
                 <div className="headerbox">
-                    <img src={logo} className="ql-logo" alt="logo" />
+                    <img src={logo} className="ql-logo" alt="logo" />     
                     <h1 style={{marginTop:0, wordSpacing: "5px", color: "#48b948"}}> DATA STORE</h1>
+                    <div style={{width:"290px"}}>
+                        <RaisedButton backgroundColor={"grey"} style={{ width: "50%" }} label="Enter Gear" />
+                        <RaisedButton style={{ width: "50%" }} label="View Index" />
+                    </div>
                 </div>
             </div>
         )
