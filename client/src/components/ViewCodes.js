ab3c8bd0c14819f24a997f17ea0cd051624a2383
Stephen Shilale <sshilale@thomasnet.com>
2018-03-05 12:24:14 -0500

    added code layout

--- client/src/components/ViewCodes.js
+++ client/src/components/ViewCodes.js
@@ -33,13 +33,27 @@ export default class ViewCodes extends React.Component {
     }
     
     render() {
+
+		const {codes} = this.props;
+
+		const Codes = codes.map((code, index) => {
+			return <div className="codes-row">
+				<div className="codes-text">{code.name}</div>
+				<div className="codes-text">{code.code}</div>
+			</div>;
+        });
+
         return(
             <div className="f-row">
 				<form className="f-col" style={{maxWidth:"500px"}} onSubmit={this.handleSubmit}>		
 					<div className="entry-row" style={{paddingTop:"20px",paddingBottom:"5px"}}>
                         <div className="entry-label"><div>User Name</div></div>
 						<TextField style={fieldstyle} name="name" onChange={this.handleInputChange} />
+                    </div><div className="entry-row" style={{paddingTop:"20px",paddingBottom:"5px"}}>
+                        <div className="entry-label"><div>Code</div></div>
+						<TextField style={fieldstyle} name="code" onChange={this.handleInputChange} />
                     </div>
+					<div className="codes-container">{Codes}</div>
                 </form>
             </div>
         )
